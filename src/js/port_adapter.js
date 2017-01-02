08f9f4b43e1e1b932913191f9e9035830f31bc8d
github1 <john.collier4@gmail.com>
2017-01-02 00:16:26 -0500

    cleanup

--- src/js/port_adapter.js
+++ src/js/port_adapter.js
@@ -57,30 +57,3 @@ app.ports.commandPort.subscribe((arr) => {
         }
     }
 });
-
-/*-
- app.ports.createNote.subscribe(() => {
- var note = {
- id: ++noteId,
- text: "",
- timeCreated: moment().format('MMMM Do YYYY, h:mm:ss a')
- };
- notes.push(note);
- app.ports.noteCreated.send(notes);
- setTimeout(() => {
- document.getElementsByClassName("note-editor")[0].focus();
- }, 100);
- });
- app.ports.modifyNote.subscribe(noteUpdate => {
- notes.forEach(note => {
- if(note.id === noteUpdate.id) {
- note.text = noteUpdate.text;
- }
- });
- app.ports.notes.send(notes);
- });
- app.ports.deleteNote.subscribe(id => {
- notes = notes.filter(note => note.id !== id);
- app.ports.notes.send(notes);
- });
- */
